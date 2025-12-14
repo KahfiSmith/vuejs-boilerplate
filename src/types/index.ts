04c8ae080771f441b4b8d1ef05fb067b8f3fcad1
Mohamad Al-Kahfi <91003256+KahfiSmith@users.noreply.github.com>
2025-12-14 19:45:02 +0700

    feat: add CSS variables for theming and responsive design, and define API types for user authentication

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type Id = string | number
+
+export interface ApiListResponse<T> {
+  data: T[]
+  total: number
+}
+
+export interface PaginatedQuery {
+  page?: number
+  pageSize?: number
+  search?: string
+}
+
+export * from './api.types'
